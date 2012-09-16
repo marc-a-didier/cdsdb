7a9cde1cf8a53e436abec5e181a0fc784922b411
marc a. didier <watson.the.burne@gmail.com>
2012-09-16 06:13:09 +0200

    Smarter version.

--- server/thinwebsrv.rb
+++ server/thinwebsrv.rb
@@ -15,65 +15,104 @@ require '../shared/dbutils'
 require '../shared/dbclassintf'
 require '../shared/utils'
 require '../shared/uiconsts'
+require '../shared/dbutils'
 
 Thread::abort_on_exception = true
 
-class PageBuilder
+GenreDBS = Struct.new(:rgenre, :sname)
 
-    def self.genres
-        page = %Q{<head><style type="text/css">
-                  a{font-family:Arial,Helvetica,sans-serif;}
-                  </style></head>}
+class Navigator
 
+    attr_accessor :genre, :artist, :record, :segment, :track
+
+    def initialize
+        @genre   = DBClassIntf.new(GenreDBS.new)
+        @artist  = ArtistDBClass.new
+        @record  = RecordDBClass.new
+        @segment = SegmentDBClass.new
+        @track   = TrackDBClass.new
+    end
+
+    def default_style
+        %Q{<head><style type="text/css">
+           a{font-family:Arial,Helvetica,sans-serif;}
+           </style></head>}
+    end
+
+    def home_page
+        page = default_style
         page += "<h1>Genres</h1><br><br><ul>"
         sql = "SELECT * FROM genres ORDER BY LOWER(sname)"
         DBIntf::connection.execute(sql) { |row|
-#             page += "<a href=/test?genre=#{URI::escape(row[1])}>#{CGI::escapeHTML(row[1])}</a><br>"
-            page += "<a href=/test?genre=#{row[0]}>#{CGI::escapeHTML(row[1])}</a><br/>"
+            page += "<a href=/muse?genre=#{row[0]}>#{CGI::escapeHTML(row[1])}</a><br/>"
         }
         page += "</ul>"
         return page
     end
 
-    def self.artists_by_genre(rgenre)
-        page = "<h1>Artists</h1><br><br><ul>"
+    def artists_by_genre(rgenre)
+        @genre.ref_load(rgenre)
+
+        page = default_style
+        page += "<h1>Artists</h1><br><br><ul>"
         sql = %Q{SELECT DISTINCT(artists.rartist), artists.sname FROM artists
                     INNER JOIN segments ON segments.rartist = artists.rartist
                     INNER JOIN records ON records.rrecord = segments.rrecord
                     WHERE records.rgenre=#{rgenre} ORDER BY LOWER(artists.sname)}
         DBIntf::connection.execute(sql) { |row|
-            page += "<a href=/test?artist=#{row[0]}&agenre=#{rgenre}>#{CGI::escapeHTML(row[1])}</a><br>"
+            page += "<a href=/muse?artist=#{row[0]}>#{CGI::escapeHTML(row[1])}</a><br>"
         }
         page += "</ul>"
         return page
     end
 
-    def self.records_by_artist_and_genre(rartist, rgenre)
-        page = "<h1>Records</h1><br><br><ul>"
+    def records_by_artist(rartist)
+        @artist.ref_load(rartist)
+
+        page = default_style
+        page += %Q{<style type="text/css">p.ex1{font:15px arial,sans-serif;}</style>}
+
+        page += %Q{<h1>Records</h1><br><br><table border="1">}
         sql = %Q{SELECT DISTINCT(rrecord) FROM segments WHERE rartist=#{rartist};}
         record = RecordDBClass.new
         DBIntf::connection.execute(sql) { |segment|
             record.ref_load(segment[0])
             image_file = Utils::get_cover_file_name(record.rrecord, 0, record.irecsymlink)
             image_file = Cfg::instance.covers_dir+"default.png" if image_file.empty?
-#             page += %Q{<img src=#{URI::escape("file://"+image_file)} width="128" height="128" />}
-            page += %Q{<img src="/image?ref=#{image_file}" width="128" height="128" />}
-            page += "<a href=/test?record=#{record.rrecord}>#{CGI::escapeHTML(record.stitle)}</a><br>"
+            page += "<tr>"
+            page += %Q{<td><img src="/image?ref=#{URI::escape(image_file)}" width="128" height="128" /></td>}
+            page += "<td>"
+            page += "<a href=/muse?record=#{record.rrecord}>#{CGI::escapeHTML(record.stitle)}</a>"
+            page += %Q{<p class="ex1">Year: #{record.iyear}<br/>}
+            page += %Q{Label: #{CGI::escapeHTML(DBUtils::name_from_id(record.rlabel, "label"))}<br/>}
+            page += %Q{Catalog: #{record.scatalog}<br/>}
+            page += %Q{Play time: #{Utils::format_ms_length(record.iplaytime)}}
+            page += "</p>"
+            page += "</td>"
+            page += "</tr>"
         }
-        page += "</ul>"
+        page += "</table>"
         return page
     end
 
-    def self.tracks_by_record(rsegment)
-        page = "<h1>Tracks</h1><br><br><ul>"
-        sql = "SELECT * FROM tracks WHERE rsegment=#{rsegment} ORDER BY iorder"
+    def tracks_by_record(rrecord)
+        page = default_style
+        page += "<h1>Tracks</h1><br><br><ul>"
+        sql = %Q{SELECT tracks.rtrack, tracks.iorder, segments.rrecord FROM tracks
+                 INNER JOIN segments ON tracks.rsegment=segments.rsegment
+                 WHERE segments.rrecord=#{rrecord} AND segments.rartist=#{@artist.rartist}
+                 ORDER BY tracks.iorder}
         DBIntf::connection.execute(sql) { |row|
-            page += "<a href=/test?track=#{row[0]}>#{row[3]} - #{CGI::escapeHTML(row[5])}</a><br>"
+            @track.ref_load(row[0])
+            page += "<a href=/muse?track=#{@track.rtrack}>#{@track.iorder} - #{CGI::escapeHTML(@track.stitle)}</a><br>"
         }
         page += "</ul>"
         return page
     end
+end
 
+class NavMgr < Navigator
+    include Singleton
 end
 
 class ImageProvider
@@ -89,21 +128,22 @@ end
 
 class SimpleAdapter
   def call(env)
+p env
     params = CGI::parse(env["QUERY_STRING"])
 p params
 
     ret_code = 200
     if params.empty?
-        page = PageBuilder::genres
+        page = NavMgr::instance.home_page
     elsif !params["genre"].empty?
-        page = PageBuilder::artists_by_genre(params["genre"][0])
+        page = NavMgr::instance.artists_by_genre(params["genre"][0])
     elsif !params["artist"].empty?
-        page = PageBuilder::records_by_artist_and_genre(params["artist"][0], params["agenre"][0])
+        page = NavMgr::instance.records_by_artist(params["artist"][0])
     elsif !params["record"].empty?
-        page = PageBuilder::tracks_by_record(params["record"][0])
+        page = NavMgr::instance.tracks_by_record(params["record"][0])
     else
         ret_code = 404
-        page = "Sorry guy..."
+        page = "Fuck you up..."
     end
     body = [page]
 
@@ -123,7 +163,7 @@ Cfg::instance.load
 
 Thin::Server.start('0.0.0.0', 7125) do
   use Rack::CommonLogger
-  map '/test' do
+  map '/muse' do
     run SimpleAdapter.new
   end
   map '/image' do
