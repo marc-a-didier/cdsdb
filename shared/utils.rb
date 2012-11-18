fa39e5b9e775b5f52fc664112d1cc1331f632fc0
marc a. didier <watson.the.burne@gmail.com>
2012-11-18 21:24:52 +0100

    Methods to colorize console strings.

--- shared/utils.rb
+++ shared/utils.rb
@@ -55,6 +55,25 @@ class String
         end
         return dt
     end
+
+
+    # Colorization of ANSI console
+    def colorize(color_code)
+        "\e[#{color_code}m#{self}\e[0m"
+    end
+
+    def black;   colorize(30); end
+    def red;     colorize(31); end
+    def green;   colorize(32); end
+    def brown;   colorize(33); end
+    def blue;    colorize(34); end
+    def magenta; colorize(35); end
+    def cyan;    colorize(36); end
+    def gray;    colorize(37); end
+
+    def bold;    colorize(1);  end
+    def blink;   colorize(5);  end
+    def reverse; colorize(7);  end
 end
 
 SEC_MS_LENGTH  = 1000
