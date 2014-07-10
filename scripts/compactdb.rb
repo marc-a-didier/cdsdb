db66a8d5f13abb1a0633429b670f7c127bc5cbd0
marc a. didier <watson.the.burne@gmail.com>
2014-07-10 06:27:18 +0200

    Updated to 6.0.

--- scripts/compactdb.rb
+++ scripts/compactdb.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 #
-# Unload/reload database -> compact it
 #
 
 require 'sqlite3'
-require 'arrayfields'
+require 'fileutils'
+
 
 class String
     def to_sql
@@ -13,35 +13,50 @@ class String
     end
 end
 
-NUM_TYPES = ["INTEGER", "SMALLINT"]
-
-$db3 = SQLite3::Database.new("cds5.2.db")
-$db3.execute("PRAGMA synchronous=OFF;")
-
-$db4 = SQLite3::Database.new("cds5.2.new.db")
-$db4.execute('PRAGMA synchronous=OFF;')
-$db4.execute('PRAGMA encoding="UTF-8";')
-
-if ARGV[0] == "--create"
-    $sql = ""
-    IO.foreach("sqlitecds5.0.sql") { |line| $sql += line.chomp }
-    $db4.execute_batch($sql)
+class Fixnum
+    def to_sql
+        return self.to_s
+    end
 end
+
+class Float
+    def to_sql
+        return self.to_s
+    end
+end
+
+
+path = "../../db/"
+
+dest = path+"cds6.0.compacted.db"
+
+$src = SQLite3::Database.new(path+"cds6.0.db")
+$src.execute("PRAGMA synchronous=OFF;")
+
+FileUtils.rm(dest) if File.exists?(dest)
+$dst = SQLite3::Database.new(dest)
+$dst.execute('PRAGMA synchronous=OFF;')
+$dst.execute('PRAGMA encoding="UTF-8";')
+
+sql = ""
+IO.foreach("./sqlitecds6.0.sql") { |line| sql += line.chomp }
+$dst.execute_batch(sql)
 
 def dup_table(table) # Copy table as it, that is there are no change
-    $db4.execute("DELETE FROM #{table};")
-    $db4.execute("BEGIN TRANSACTION;")
-    $db3.execute("SELECT * FROM #{table}") do |row|
+    $dst.execute("DELETE FROM #{table};")
+    $dst.execute("BEGIN TRANSACTION;")
+    $src.execute("SELECT * FROM #{table}") do |row|
         sql = "INSERT INTO #{table} VALUES ("
-        row.each_with_index do |val, i|
-            NUM_TYPES.include?(row.types[i].upcase) ? sql += val+"," : sql += val.to_sql+","
-        end
+        row.each { |val| sql += val.to_sql+"," }
+
         sql = sql[0..-2]+");"
 puts sql
-        $db4.execute(sql)
+        $dst.execute(sql)
     end
-    $db4.execute("COMMIT;")
+    $dst.execute("COMMIT;")
 end
 
-["medias", "collections", "labels", "genres", "plists", "pltracks", "logtracks", "origins"].each { |table| dup_table(table) }
-["artists", "records", "segments", "tracks"].each { |table| dup_table(table) }
+["collections", "medias", "genres", "labels",
+ "plists", "pltracks", "origins",
+ "artists", "records", "segments", "tracks",
+ "hostnames", "logtracks", "filters"].each { |table| dup_table(table) }
