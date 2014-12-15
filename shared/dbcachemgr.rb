1d4469c0f77c116d8979262c92417e231bc8bd09
marc a. didier <watson.the.burne@gmail.com>
2014-12-15 18:12:19 +0100

    Ability to trace cache hits/misses.

--- shared/dbcachemgr.rb
+++ shared/dbcachemgr.rb
@@ -7,13 +7,13 @@
 # It automatically load rows from DB when the refered row is not already in cache.
 #
 
-EMPTY_STR = ""
-
 AudioInfos = Struct.new(:status, :file)
 
 class DBCache
 
     include Singleton
+
+    TRACE_CACHE = false
 
     def initialize
         @artists     = {}
@@ -29,44 +29,46 @@ class DBCache
         # Keep tracks of audio file status for a track, avoiding to
         # repeatedly ask to the server if the track exists in client mode.
         # Closely related to the track cache.
-#         @audio_status = {}
         @audio = {}
     end
 
     def artist(rartist)
-#         @artists[rartist] = ArtistDBClass.new.ref_load(rartist) if @artists[rartist].nil?
         if @artists[rartist].nil?
             @artists[rartist] = ArtistDBClass.new.ref_load(rartist)
-# TRACE.debug("Artist cache loaded key #{rartist}, size=#{@artists.size}")
+            TRACE.debug("Artist cache MISS for key #{rartist}, size=#{@artists.size}") if TRACE_CACHE
+        else
+            TRACE.debug("Artist cache HIT for key #{rartist}, size=#{@artists.size}") if TRACE_CACHE
         end
         return @artists[rartist]
     end
 
     def record(rrecord)
-#         @records[rrecord] = RecordDBClass.new.ref_load(rrecord) if @records[rrecord].nil?
         if @records[rrecord].nil?
             @records[rrecord] = RecordDBClass.new.ref_load(rrecord)
-# TRACE.debug("Record cache loaded key #{rrecord}, size=#{@records.size}")
+            TRACE.debug("Record cache MISS for key #{rrecord}, size=#{@records.size}") if TRACE_CACHE
+        else
+            TRACE.debug("Record cache HIT for key #{rrecord}, size=#{@records.size}") if TRACE_CACHE
         end
         return @records[rrecord]
     end
 
     def segment(rsegment)
-#         @segments[rsegment] = SegmentDBClass.new.ref_load(rsegment) if @segments[rsegment].nil?
         if @segments[rsegment].nil?
             @segments[rsegment] = SegmentDBClass.new.ref_load(rsegment)
-# TRACE.debug("Segment cache loaded key #{rsegment}, size=#{@segments.size}")
+            TRACE.debug("Segment cache MISS for key #{rsegment}, size=#{@segments.size}") if TRACE_CACHE
+        else
+            TRACE.debug("Segment cache HIT for key #{rsegment}, size=#{@segments.size}") if TRACE_CACHE
         end
         return @segments[rsegment]
     end
 
     def track(rtrack)
-#         @tracks[rtrack] = TrackDBClass.new.ref_load(rtrack) if @tracks[rtrack].nil?
         if @tracks[rtrack].nil?
             @tracks[rtrack] = TrackDBClass.new.ref_load(rtrack)
-#             @audio_status[rtrack] = 4 # AudioLink::UNKNOWN default value
-            @audio[rtrack] = AudioInfos.new(4, EMPTY_STR)
-# TRACE.debug("Track cache loaded key #{rtrack}, size=#{@tracks.size}")
+            @audio[rtrack] = AudioInfos.new(4, nil)
+            TRACE.debug("Track cache MISS for key #{rtrack}, size=#{@tracks.size}") if TRACE_CACHE
+        else
+            TRACE.debug("Track cache HIT for key #{rtrack}, size=#{@tracks.size}") if TRACE_CACHE
         end
         return @tracks[rtrack]
     end
@@ -105,32 +107,26 @@ class DBCache
     end
 
     def set_audio_status(rtrack, status)
-#         @audio_status[rtrack] = status
         @audio[rtrack].status = status
     end
 
     def audio_status(rtrack)
-#         track(rtrack) unless @audio_status[rtrack]
         track(rtrack) unless @audio[rtrack]
         @audio[rtrack].status = 4 unless @audio[rtrack] # Unknown status if not in cache
-#         @audio_status[rtrack] = 4 unless @audio_status[rtrack] # Unknown status if not in cache
         return @audio[rtrack].status
-#         return @audio_status[rtrack]
     end
 
     def clear
 #         instance_variables.each { |cache| cache.clear } # Marche pas!!!???
-#         [@artists, @records, @segments, @tracks, @audio_status,
         [@artists, @records, @segments, @tracks, @audio,
          @genres, @labels, @medias, @collections, @origins].each { |cache| cache.clear }
-TRACE.debug("ALL CACHES cleared")
+        TRACE.debug("ALL CACHES cleared") if TRACE_CACHE
     end
 
     # Set audio status from a status to another
     # Primary use is when switching from local mode to client mode (not found -> unknown)
     # and from client to local mode (on server -> not found)
     def set_audio_status_from_to(from_value, to_value)
-#         @audio_status.each { |key, value| @audio_status[key] = to_value if value == from_value }
         @audio.each { |key, value| value.status = to_value if value.status == from_value }
     end
 
@@ -145,7 +141,6 @@ TRACE.debug("ALL CACHES cleared")
         TRACE.debug("Media cache size=#{@medias.size}")
         TRACE.debug("Collection cache size=#{@collections.size}")
         TRACE.debug("Origin cache size=#{@origins.size}")
-#         TRACE.debug("Audio status cache size=#{@audio_status.size}")
         TRACE.debug("Audio infos cache size=#{@audio.size}")
     end
 end
