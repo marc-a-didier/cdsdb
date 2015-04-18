caef2abc9fbeb2ceeb2cffa0466353a8b98b5c37
marc a. didier <watson.the.burne@gmail.com>
2015-04-18 08:58:56 +0200

    Updated for new server

--- src/server/reload_hosts.rb
+++ src/server/reload_hosts.rb
@@ -3,9 +3,9 @@
 require 'socket'
 
 begin
-    socket = TCPSocket.new('madD510', 32666)
+    socket = TCPSocket.new('madAM1H', 32666)
 rescue Errno::ECONNREFUSED => ex
     puts "Connection error (#{ex.class} : #{ex})."
 end
-socket.puts("reload hosts-1")
+socket.puts("reload hosts")
 puts("Response: #{socket.gets.chomp}")
