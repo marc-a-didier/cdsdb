a2210718e2be72d35e14e514056879537f40025b
marc a. didier <watson.the.burne@gmail.com>
2015-11-02 22:13:35 +0100

    GraphStats enhancements

--- src/frontend/graphstats.rb
+++ src/frontend/graphstats.rb
@@ -1,8 +1,6 @@
 
 class DatesHandler
 
-    END_OF_DAY_OFFSET = 23*60*60+59*60+59
-
     def initialize(from_date, increment)
         @curr_date = DateTime.parse(from_date)
         @increment = increment
@@ -21,9 +19,8 @@ class DatesHandler
     end
 
     def end_date
-        t = Time.at(next_offset.to_time.to_i-1)
+        t = next_offset.prev_day
         return Time.new(t.year, t.month, t.day, 23, 59, 59).to_i
-        #next_offset.to_time.to_i #-1 #+END_OF_DAY_OFFSET).to_i
     end
 
     def next_date
@@ -37,29 +34,41 @@ class DatesHandler
             when :year  then (2010..2015)
         end
     end
-end
 
-class GraphStats
-
-    def initialize
-        @charts = []
+    def x_axis_label
+        return case @increment
+            when :day   then @curr_date.strftime('%b %-d %Y')
+            when :month then @curr_date.strftime('%b %Y')
+            when :year  then @curr_date.year.to_s
+        end
     end
 
-    def new_chart(type, data_titles, data_rows, options)
+    def period_label
+        return case @increment
+            when :day   then "Daily"
+            when :month then "Monthly"
+            when :year  then "Yearly"
+        end
+    end
+end
+
+module GraphStats
+
+    def self.new_chart(type, data_titles, data_rows, options)
         html = IO.read("../scripts/#{type}_chart.template.html")
 
         html.sub!(/___DATA_TITLES___/, data_titles)
         html.sub!(/___DATA_ROWS___/, data_rows)
         html.sub!(/___CHART_OPTS___/, options)
 
-        @charts << html
+        return html
     end
 
-    def render_charts
+    def self.render_charts(charts)
         File.open(Cfg.rsrc_dir+'dbstats.html', 'w') do |f|
             f.write('<!DOCTYPE html><head><meta charset="UTF-8">')
             body = "</head>\n<body>\n"
-            @charts.each_with_index do |chart, i|
+            charts.each_with_index do |chart, i|
                 chart.sub!(/@@id@@/, (i+1).to_s)
                 id = chart.match(/getElementById\('(.+)'\)/).captures.first
                 body << "<div id='#{id}'></div>\n"
@@ -70,14 +79,12 @@ class GraphStats
         end
     end
 
-    def genres_played_line
+    def self.genres_played_line
         genres = {}
         DBIntf.execute("SELECT sname FROM genres WHERE rgenre<>0;") { |row| genres[row[0]] = 0 }
-        dh = DatesHandler.new(Time.new(2014, 12, 1).to_s, :day)
+        dh = DatesHandler.new(Time.new(2015, 10, 1).to_s, :day)
         data = []
         dh.range.each do |i|
-            start_date = Time.new(2014, 12, i).to_i
-            end_date = Time.new(2014, 12, i, 23, 59, 59).to_i
             sql = %{
                 SELECT genres.sname, COUNT(records.rgenre) FROM logtracks
                     INNER JOIN tracks ON tracks.rtrack=logtracks.rtrack
@@ -87,7 +94,7 @@ class GraphStats
                 GROUP BY records.rgenre;
             }
             DBIntf.execute(sql) { |row| genres[row[0]] = row[1] }
-            data << [Time.at(dh.start_date).strftime('%b %-d %Y')]
+            data << [dh.x_axis_label]
             genres.each do |k, v|
                 data.last << v
                 genres[k] = 0
@@ -110,11 +117,11 @@ class GraphStats
         new_chart(:line,
                   genres.keys.unshift('Genre').to_s,
                   data.map { |row| row.to_s }.join(",\n"),
-                  "title: 'Daily play count by genre', hAxis: { title: 'Day'}, vAxis: { title: 'Play count' }")
+                  "title: '#{dh.period_label} play count by genre', vAxis: { title: 'Play count' }")
     end
 
-    def artists_col_chart
-        dh = DatesHandler.new(Time.new(2015, 10, 1).to_s, :month)
+    def self.artists_col_chart
+        dh = DatesHandler.new(Time.new(2014, 1, 1).to_s, :year)
         sql = %{
             SELECT artists.sname, COUNT(segments.rartist) AS artcount FROM logtracks
                 INNER JOIN tracks ON tracks.rtrack=logtracks.rtrack
@@ -130,7 +137,7 @@ class GraphStats
                   "title: '50 most played artists from #{dh.start_date.to_std_date} to #{dh.end_date.to_std_date}'")
     end
 
-    def tags_col_chart
+    def self.tags_col_chart
         start_date = Time.new(2014, 1, 1).to_i
         end_date = Time.new(2014, 12, 31, 23, 59, 59).to_i
 
@@ -146,13 +153,14 @@ class GraphStats
             Qualifiers::TAGS.size.times { |i| tags[i][1] += 1 if row[0] & (1 << i) != 0 }
         end
 
-        @html.sub!(/___CC_ROWS___/, tags.map { |tag| tag.to_s }.join(",\n"))
+#         @html.sub!(/___CC_ROWS___/, tags.map { |tag| tag.to_s }.join(",\n"))
     end
 
-    def ratings_line_chart
+    def self.ratings_line_chart
         ratings = []
-        dh = DatesHandler.new(Time.new(2014).to_s, :month)
+        dh = DatesHandler.new(Time.new(2010).to_s, :year)
         dh.range.each do
+puts "start=#{Time.at(dh.start_date)} - #{Time.at(dh.end_date)}"
             sql = %{
                 SELECT tracks.irating FROM logtracks
                     INNER JOIN tracks ON tracks.rtrack=logtracks.rtrack
@@ -160,31 +168,49 @@ class GraphStats
             }
 
             ratings << Array.new(Qualifiers::RATINGS.size+1, 0)
-            ratings.last[0] = Time.at(dh.start_date).strftime('%b %Y')
+            ratings.last[0] = dh.x_axis_label
             DBIntf.execute(sql) do |row|
-                Qualifiers::RATINGS.size.times { |i| ratings.last[row[0]+1] += 1 }
+                ratings.last[row[0]+1] += 1
             end
             dh.next_date
         end
         new_chart(:line,
                   "['Ratings', "+Qualifiers::RATINGS.map { |rating| '"'+rating+'"' }.join(',')+']',
                   ratings.map { |rating| rating.to_s }.join(",\n"),
-                  "title: 'Monthly play count by rating', hAxis: { title: 'Periods'}, vAxis: { title: 'Play count' }")
+                  "title: '#{dh.period_label} play count by rating', vAxis: { title: 'Play count' }")
     end
 
-    def tags_line_chart
-        tags = []
-        dh = DatesHandler.new(Time.new(2014).to_s, :month)
+    def self.played_tracks
+        data = []
+        dh = DatesHandler.new(Time.new(2010).to_s, :year)
         dh.range.each do
+            sql = %{
+                SELECT COUNT(rtrack) FROM logtracks
+                WHERE logtracks.idateplayed >= #{dh.start_date} AND logtracks.idateplayed <= #{dh.end_date};
+            }
+            data << [dh.x_axis_label, DBIntf.get_first_value(sql)]
+            dh.next_date
+        end
+        new_chart(:column,
+                  "['Year', 'Play count']",
+                  data.map { |entry| entry.to_s }.join(",\n"),
+                  "title: '#{dh.period_label} played tracks'")
+    end
+
+    def self.tags_line_chart
+        tags = []
+        dh = DatesHandler.new(Time.new(2014, 2, 1).to_s, :day)
+        dh.range.each do
+puts "start=#{Time.at(dh.start_date)} - #{Time.at(dh.end_date)}"
             sql = %{
                 SELECT tracks.itags FROM logtracks
                     INNER JOIN tracks ON tracks.rtrack=logtracks.rtrack
                 WHERE logtracks.idateplayed >= #{dh.start_date} AND logtracks.idateplayed <= #{dh.end_date} AND
-                    tracks.itags<>0;
+                      tracks.itags<>0;
             }
 
             tags << Array.new(Qualifiers::TAGS.size+1, 0)
-            tags.last[0] = Time.at(dh.start_date).strftime('%b %Y')
+            tags.last[0] = dh.x_axis_label
             DBIntf.execute(sql) do |row|
                 Qualifiers::TAGS.size.times { |i| tags.last[i+1] += 1 if row[0] & (1 << i) != 0 }
             end
@@ -193,10 +219,16 @@ class GraphStats
         new_chart(:line,
                   "['Tags', "+Qualifiers::TAGS.map { |tag| '"'+tag+'"' }.join(',')+']',
                   tags.map { |tag| tag.to_s }.join(",\n"),
-                  "title: 'Monthly play count by tag', hAxis: { title: 'Periods'}, vAxis: { title: 'Play count' }")
-        ratings_line_chart
-        genres_played_line
-        artists_col_chart
-        render_charts
+                  "title: '#{dh.period_label} play count by tag', vAxis: { title: 'Play count' }")
+    end
+
+    def self.draw_charts
+        charts = []
+        charts << tags_line_chart
+        charts << ratings_line_chart
+        charts << genres_played_line
+        charts << played_tracks
+        charts << artists_col_chart
+        render_charts(charts)
     end
 end
